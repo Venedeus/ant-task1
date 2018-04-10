3135273b5f552e644d5ab1a39c09ba01e5d288d5
Evgeniy Shvetsov <venedey.e@gmail.com>
2018-04-10 17:51:44 +0300

    First commit in hotfix#1 branch.

--- src/java/classes/HelloWorld.java
+++ src/java/classes/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		System.out.println("We are working in the master-branch.");
+		System.out.println("Hotfix#1 is done in the master-branch.");
 	}
 }
